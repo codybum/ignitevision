425b99cae00ccb3730dcacf1a0145ef1fced1843
codybum <cbumgardner@gmail.com>
2021-09-17 13:23:17 -0400

    update

--- utils.py
+++ utils.py
@@ -18,19 +18,6 @@ test_transform = Compose(
      Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ])
 
-
-def get_train_test_datasets_old(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
-        download = True
-    else:
-        download = True if len(os.listdir(path)) < 1 else False
-
-    train_ds = datasets.CIFAR10(root=path, train=True, download=download, transform=train_transform)
-    test_ds = datasets.CIFAR10(root=path, train=False, download=False, transform=test_transform)
-
-    return train_ds, test_ds
-
 def get_train_test_datasets(path):
 
     train_ds = datasets.ImageFolder(os.path.join(path, 'train'),train_transform)
